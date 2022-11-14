64d6d3c3ddd168de15d1ae8a3428cbc50097b319
ThursdayActivity-13-10-2022 <115717468+Jgoud9611@users.noreply.github.com>
2022-11-14 12:47:54 +0530

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,13 @@
+package thursday_activity5_10_nov;
+
+public class Circle extends Shape{
+	
+		void draw(){
+			
+			System.out.println("Drawing circle");
+		}
+		void erase(){
+			System.out.println("Erasing circle");
+			
+		}
+}
