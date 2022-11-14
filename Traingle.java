c41b174141fe6739cd6e61f2cfcca5d3f3d7dc64
ThursdayActivity-13-10-2022 <115717468+Jgoud9611@users.noreply.github.com>
2022-11-14 12:48:42 +0530

    Create Traingle.java

---
+++ Traingle.java
@@ -0,0 +1,11 @@
+package thursday_activity5_10_nov;
+
+public class Traingle extends Shape{
+		void draw(){
+			System.out.println("Drawing Traingle");
+		}
+		void erase(){
+			System.out.println("Erasing Traingle");
+			
+		}
+}
