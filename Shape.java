7467084fee0e82e1235a651a078d9ea9f0a28a1d
ThursdayActivity-13-10-2022 <115717468+Jgoud9611@users.noreply.github.com>
2022-11-14 12:50:09 +0530

    Create Shape.java

---
+++ Shape.java
@@ -0,0 +1,14 @@
+package thursday_activity5_10_nov;
+
+public class Shape {
+	void draw()
+	{
+		System.out.println("Drawing shape");
+	}
+	void erase()
+	{
+		System.out.println("Erasing Shape");
+	}
+
+	
+}
