791a7430357ab4ae718ef68fc21232854cae5fbe
ThursdayActivity-13-10-2022 <115717468+Jgoud9611@users.noreply.github.com>
2022-11-14 12:54:24 +0530

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,12 @@
+package thursday_activity5_10_nov;
+
+public class Square extends Shape{
+	
+	void draw(){
+		System.out.println("Drawing Square");
+	}
+	void erase(){
+		System.out.println("Erasing Square");
+		
+	}
+}
