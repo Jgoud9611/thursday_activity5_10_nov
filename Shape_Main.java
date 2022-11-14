d76f8882151222ba905d659f7051104e519a725d
ThursdayActivity-13-10-2022 <115717468+Jgoud9611@users.noreply.github.com>
2022-11-14 12:49:29 +0530

    Create Shape_Main.java

---
+++ Shape_Main.java
@@ -0,0 +1,18 @@
+package thursday_activity5_10_nov;
+
+public class Shape_Main {
+
+	public static void main(String[] args) {
+		Shape c= new Circle();
+		Shape t= new Traingle();
+		Shape s=new Square();
+		c.draw();
+		c.erase();
+		t.draw();
+		t.erase();
+		s.draw();
+		s.erase();
+
+	}
+
+}
